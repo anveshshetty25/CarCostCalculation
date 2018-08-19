076d91ef08245fc4ca746fb5445fe8d39ec98387
Anwesh Valleshetti <anveshshetty25@gmail.com>
2018-08-18 23:24:51 -0500

    [MOD]-Modified the UI and added carousal display

--- src/main/java/com/dynatrace/carcostcalculation/service/CarCostCalculation.java
+++ src/main/java/com/dynatrace/carcostcalculation/service/CarCostCalculation.java
@@ -18,13 +18,6 @@ import org.apache.log4j.Logger;
 @Path("")
 public class CarCostCalculation {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     * @throws Exception 
-     */
 	public static final String CAR_COST = "cost";
 	public static final String ENGINE_TYPE = "v8";
 	public static final String TRANSMISSION_TYPE = "automatic";
@@ -45,101 +38,111 @@ public class CarCostCalculation {
 		put(SUV,30000);
 		put(LUXARY_SEDAN,35000);
 	}};
- 	private static final Map<String, Map<String,Integer>> OptionsLookup = new HashMap<String, Map<String,Integer>>()
-    {{
-    	put(COUPE,new HashMap<String,Integer>(){{
-    		put(ENGINE_TYPE,5000);
-    		put(TRANSMISSION_TYPE,1000);
-    		put(MUSIC_TYPE,1000);
-    		put(SUNROOF,1000);
-    		put(NAVIGATION,1000);
-    		
-    	}});
-    	put(TRUCK,new HashMap<String,Integer>(){{
-    		put(ENGINE_TYPE,6000);
-    		put(TRANSMISSION_TYPE,1500);
-    		put(MUSIC_TYPE,1100);
-    		put(SUNROOF,1000);
-    		put(NAVIGATION,1000);
-    		put(TOW_PACKAGE,550);
-    		
-    	}});
-    	put(SUV,new HashMap<String,Integer>(){{
-    		put(ENGINE_TYPE,5500);
-    		put(TRANSMISSION_TYPE,1200);
-    		put(MUSIC_TYPE,1500);
-    		put(SUNROOF,1000);
-    		put(NAVIGATION,1000);
-    		put(TOW_PACKAGE,500);
-    		
-    	}});
-    	put(LUXARY_SEDAN,new HashMap<String,Integer>(){{
-    		put(ENGINE_TYPE,25000);
-    		put(TRANSMISSION_TYPE,1200);
-    		put(MUSIC_TYPE,1500);
-    		put(SUNROOF,1000);
-    		put(NAVIGATION,1000);
-    		put(TOW_PACKAGE,500);
-    	}});
-    }};
-    
-    
-    @GET
-    @Path("{carType}/{options}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getCarCost(@PathParam("carType") String carType,@PathParam("options") String options) throws Exception {
-    	double carCost = 0.0;
-    		String[] carOptions={};
-    	if(options!="default"){
-    	 carOptions = options.split(",");
-    	 logger.info("User selected of car type "+carType+"with"+carOptions);
-    	}
-        
-        if(carslookup.containsKey(carType)){
-        	carCost += carslookup.get(carType);
-        	for(String option:carOptions){
-        	if(OptionsLookup.get(carType).containsKey(option)){
-        		carCost += OptionsLookup.get(carType).get(option);
-        	}else if(option.length()!=0){
-        		Set<String> allowedCarTypes = OptionsLookup.get(carType).keySet();
-        		logger.error(carType+" doesn't support the options you selected please select options from these "+allowedCarTypes);
-        		return carType+" doesn't support the options you selected please select options from these "+allowedCarTypes;
-        	}
-        	}
-        	if (carCost > 30000 && carCost <= 60000) {
-                // luxury tax
-                carCost += 850;
-            } else if (carCost > 60000) {
-                // extra luxury tax
-                carCost += 1000;
-            }
-            double tax = 0;
-            //looping two times
-            tax = slowTaxCalculationMethod();
-            for (String gasGuzzler : gasGuzzlers) {
-                if (gasGuzzler.equals(carType)) {
-                    // adding gas guzzler tax
-                    tax += 1000;
-                    break;
-                }
-            }
-            carCost += tax;
-        }else{
-        	logger.error("The Car Type "+carType+" is not available please select car types from the following "+carslookup.keySet());
-        	return "The Car Type "+carType+" is not available please select car types from the following "+carslookup.keySet();
-        }
-    	return "The car value of "+carType+" is "+carCost; 
-    }
-    
-    private static double slowTaxCalculationMethod() {
-        // the Thread.sleep cannot be removed
-        try {
-            Thread.sleep((long) 0.1);
-        } catch (Exception e) {
-            // Do nothing
-        }
-        return 500;
-    }
+	private static final Map<String, Map<String,Integer>> OptionsLookup = new HashMap<String, Map<String,Integer>>()
+	{{
+		put(COUPE,new HashMap<String,Integer>(){{
+			put(ENGINE_TYPE,5000);
+			put(TRANSMISSION_TYPE,1000);
+			put(MUSIC_TYPE,1000);
+			put(SUNROOF,1000);
+			put(NAVIGATION,1000);
+
+		}});
+		put(TRUCK,new HashMap<String,Integer>(){{
+			put(ENGINE_TYPE,6000);
+			put(TRANSMISSION_TYPE,1500);
+			put(MUSIC_TYPE,1100);
+			put(SUNROOF,1000);
+			put(NAVIGATION,1000);
+			put(TOW_PACKAGE,550);
+
+		}});
+		put(SUV,new HashMap<String,Integer>(){{
+			put(ENGINE_TYPE,5500);
+			put(TRANSMISSION_TYPE,1200);
+			put(MUSIC_TYPE,1500);
+			put(SUNROOF,1000);
+			put(NAVIGATION,1000);
+			put(TOW_PACKAGE,500);
+
+		}});
+		put(LUXARY_SEDAN,new HashMap<String,Integer>(){{
+			put(ENGINE_TYPE,25000);
+			put(TRANSMISSION_TYPE,1200);
+			put(MUSIC_TYPE,1500);
+			put(SUNROOF,1000);
+			put(NAVIGATION,1000);
+			put(TOW_PACKAGE,500);
+		}});
+	}};
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent
+	 * to the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 * @throws Exception 
+	 */
+	@GET
+	@Path("{carType}/{options}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getCarCost(@PathParam("carType") String carType,@PathParam("options") String options) throws Exception {
+		double carCost = 0.0;
+		String[] carOptions={};
+		//If user doesn't select any options then the value will be default and carOptions array is empty
+		if(!options.equals("default")){
+			carOptions = options.split(",");
+			logger.info("User selected of car type "+carType+"with"+carOptions);
+		}
+
+		//checks the car type and options selected by user and adds the respective cost
+		if(carslookup.containsKey(carType)){
+			carCost += carslookup.get(carType);
+			for(String option:carOptions){
+				if(OptionsLookup.get(carType).containsKey(option)){
+					carCost += OptionsLookup.get(carType).get(option);
+				}else {
+					Set<String> allowedCarTypes = OptionsLookup.get(carType).keySet();
+					logger.error(carType+" doesn't support the options you selected please select options from these "+allowedCarTypes);
+					return carType+" doesn't support the options you selected please select options from these "+allowedCarTypes;
+				}
+			}
+			logger.debug("The car value before adding tax"+carCost);
+			if (carCost > 30000 && carCost <= 60000) {
+				// luxury tax
+				carCost += 850;
+			} else if (carCost > 60000) {
+				// extra luxury tax
+				carCost += 1000;
+			}
+			double tax = 0;
+			//looping two times
+			tax = slowTaxCalculationMethod();
+			for (String gasGuzzler : gasGuzzlers) {
+				if (gasGuzzler.equals(carType)) {
+					// adding gas guzzler tax
+					tax += 1000;
+					break;
+				}
+			}
+			carCost += tax;
+			logger.debug("The car value after adding tax"+carCost);
+		}else{
+			logger.error("The Car Type "+carType+" is not available please select car types from the following "+carslookup.keySet());
+			return "The Car Type "+carType+" is not available please select car types from the following "+carslookup.keySet();
+		}
+		logger.info("The car value of "+carType+" is "+carCost);
+		return "The car value of "+carType+" is "+carCost; 
+	}
+
+	private static double slowTaxCalculationMethod() {
+		try {
+			Thread.sleep((long) 0.1);
+		} catch (Exception e) {
+			logger.error("Exception due to"+e);
+		}
+		return 500;
+	}
 
 
 
